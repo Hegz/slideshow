6b48603d3d2f2c5207c3db04b2cbb1890e7da173
Adam Fairbrother <afairbrother@sd73.bc.ca>
2011-02-09 14:35:31 -0800

    Minor fix to uninstall

--- install.pl
+++ install.pl
@@ -3,11 +3,11 @@
 #
 #         FILE:  install.pl
 #
-#        USAGE:  ./install.pl  
+#        USAGE:  ./install.pl
 #
 #  DESCRIPTION:  Perform the nessiary steps to setup the slideshow system
 #
-# REQUIREMENTS:  Run from the same folder as the contents of the archive with 
+# REQUIREMENTS:  Run from the same folder as the contents of the archive with
 #                Root permissions (sudo).
 #
 #       AUTHOR:  Adam Fairbrother (AF), afairbrother@sd73.bc.ca
@@ -26,48 +26,60 @@ use options;
 use Getopt::Long;
 
 #Setup Command line flags
-my $install = 0;
-my $update = 0;
+my $install   = 0;
+my $update    = 0;
 my $uninstall = 0;
-GetOptions ( 'install'   => \$install,
-		'update'    => \$update,
-		'uninstall' => \$uninstall);
+GetOptions(
+	'install'   => \$install,
+	'update'    => \$update,
+	'uninstall' => \$uninstall
+);
 
-unless ($install || $update || $uninstall){
+unless ( $install || $update || $uninstall ) {
+
 	# Display Usage information
 	print "No flags detected\n";
 	exit;
 }
 
-if (($install + $update + $uninstall) > 1 ){
+if ( ( $install + $update + $uninstall ) > 1 ) {
+
 	# Display usage information
 	print "Multiple flags detected\n";
 	exit;
 }
 
 if ($install) {
+
 	# Verify the config file.
 	unless ($installrdy) {
-		print "Please edit the config file options.pm to ensure that eveything is setup correctly.\n";
+		print
+			"Please edit the config file options.pm to ensure that eveything is setup correctly.\n";
 		print "Install Aborting, Unsuccessful\n";
 		exit;
 	}
 	unless ($machine) {
-		print "You will need to set the machine dns name for the slideshow to run.\n";
+		print
+			"You will need to set the machine dns name for the slideshow to run.\n";
 		print "Install Aborting, Unsuccessful\n";
 		exit;
 	}
+
 	# make sure we're running as root, or with sudo
-	unless ($ENV{'USER'} eq 'root'){
-		print "This script requires root privilages.  Please run with sudo.\n";
+	unless ( $ENV{'USER'} eq 'root' ) {
+		print
+			"This script requires root privilages.  Please run with sudo.\n";
 		print "Install Aborting, Unsuccessful\n";
 		exit;
 	}
+
 	# Check to see that $showuser isn't already on the system
-	open my $passwdfile, '<', '/etc/passwd' or die("error opening passwd file $!\n");
+	open my $passwdfile, '<', '/etc/passwd'
+		or die("error opening passwd file $!\n");
 	while (<$passwdfile>) {
-		if (m/$showuser/){
-			print "the user account $showuser is already active on the system.  Is the slideshow account already setup?\n";
+		if (m/$showuser/) {
+			print
+				"the user account $showuser is already active on the system.  Is the slideshow account already setup?\n";
 			print "Install Aborting, Unsuccessful\n";
 			exit;
 		}
@@ -75,43 +87,51 @@ if ($install) {
 
 	# check for the source files in the current dir.
 	my $currentdir = getcwd;
-	opendir my $source, "$currentdir" or die("Error opening current directory $!\n");
-	my @localfiles = readdir($source);
+	opendir my $source, "$currentdir"
+		or die("Error opening current directory $!\n");
+	my @localfiles    = readdir($source);
 	my $correctfolder = 0;
 	foreach (@localfiles) {
-		if (m/install.pl/){
+		if (m/install.pl/) {
 			$correctfolder = 1;
 		}
 	}
 	close $source;
 	unless ($correctfolder) {
-		print "This script must be run from within extracted file directory.\n";
-		print "Please change to the directoy where you extracted the files and try again\n";
+		print
+			"This script must be run from within extracted file directory.\n";
+		print
+			"Please change to the directoy where you extracted the files and try again\n";
 		print "Install Aborting, Unsuccessful\n";
 		exit;
 	}
 
-
-	# begin the installation steps here, 
+	# begin the installation steps here,
 	my $autologinfile = '/var/lib/vservers/vs1/etc/gdm/autologin';
 	my $autologin;
-	open $autologin, '<', $autologinfile or die("Error opening autologin file for read ($autologinfile):$!\n");
-	while (<$autologin>){
-		if (m/(.*)\:$showuser/){
-			print "Account $showuser is already set to atologin on machine $1\n";
+	open $autologin, '<', $autologinfile
+		or die("Error opening autologin file for read ($autologinfile):$!\n");
+	while (<$autologin>) {
+		if (m/(.*)\:$showuser/) {
+			print
+				"Account $showuser is already set to atologin on machine $1\n";
 			print "Install Aborting, Unsuccessful\n";
 			exit;
 		}
 	}
 	close $autologin;
 
-	open $autologin, '>>', $autologinfile or die("Error opening autologin file for write ($autologinfile):$!\nInstall Aborting, Unsuccessful\n");
+	open $autologin, '>>', $autologinfile
+		or die(
+		"Error opening autologin file for write ($autologinfile):$!\nInstall Aborting, Unsuccessful\n"
+		);
 
 	# **** Anything that can fail during install should do so before this point ****
 
 	# Add the user to the system
 	`useradd -d $userhomedir -m -s /bin/bash -g teachers -c 'Automatic slideshow computer account' $showuser `;
 	`make -C /var/yp/`;
+
 	# Copy the files from the current dir to the users folder.
 	`cp $currentdir/* -r $userhomedir`;
 	`cp $currentdir/.??* -r $userhomedir`;
@@ -126,79 +146,96 @@ if ($install) {
 	print $autologin "$machine:$showuser\n";
 	close $autologin;
 
-	# Create the slideshow folder, and hidden folder 
-	mkpath($showdir,$convertdir);
+	# Create the slideshow folder, and hidden folder
+	mkpath( $showdir, $convertdir );
 
 	symlink $showdir, "$convertdir/showdir";
+
 	# Set permisions on the slideshow folder and the hidden folder
 	`chown $showuser\\: $showdir -R`;
 	`chmod 770 $showdir -R`;
 
 	# Update the cron scripts for the correct paths
 	{
-		local $^I = '.bak';
-		local @ARGV = ("$userhomedir/.kde/Autostart/cron-15min.sh", "$userhomedir/.kde/Autostart/cron-1min.sh");
-		while (<>){
+		local $^I   = '.bak';
+		local @ARGV = (
+			"$userhomedir/.kde/Autostart/cron-15min.sh",
+			"$userhomedir/.kde/Autostart/cron-1min.sh"
+		);
+		while (<>) {
 			s/PATH/$userhomedir/;
 			s/LOG/$logdir/;
-			print
+			print;
 		}
-		unlink ("$userhomedir/.kde/Autostart/cron-15min.sh.bak", "$userhomedir/.kde/Autostart/cron-1min.sh.bak");
+		unlink(
+			"$userhomedir/.kde/Autostart/cron-15min.sh.bak",
+			"$userhomedir/.kde/Autostart/cron-1min.sh.bak"
+		);
 	}
 
 	# Update the .xscreensaver file with the correct slideshow path
 	{
-		local $^I = '.bak';
+		local $^I   = '.bak';
 		local @ARGV = ("$userhomedir/.xscreensaver");
-		while (<>){
+		while (<>) {
 			s/PATH/$convertdir/;
 			print;
 		}
-		unlink ("$userhomedir/.xscreensaver.bak");
+		unlink("$userhomedir/.xscreensaver.bak");
 	}
 
 	# Run the mkdoc script to write a quick users guide to the slideshow folder
-	eval {require "mkdoc.pl"};
+	eval { require "mkdoc.pl" };
 
 	print "Install Successful\n";
-	print "Please restart the slideshow machine ($machine) to complete the installation\n";
+	print
+		"Please restart the slideshow machine ($machine) to complete the installation\n";
 	exit;
 }
 elsif ($uninstall) {
+
 	# run install from $showuser $HOME
 	my $currentdir = getcwd;
-	if ($currentdir ne $userhomedir){
-		print "Please run the uninstall script from the Slidshow users home folder\n";
+	if ( $currentdir ne $userhomedir ) {
+		print
+			"Please run the uninstall script from the Slidshow users home folder\n";
 		print "Uninstall Aborting, Unsuccessful\n";
 		exit;
 	}
+
 	# Make sure we're running with root privalages, or sudo
-	unless ($ENV{'USER'} eq 'root'){
-		print "This script requires root privilages.  Please run with sudo.\n";
+	unless ( $ENV{'USER'} eq 'root' ) {
+		print
+			"This script requires root privilages.  Please run with sudo.\n";
 		print "Unnstall Aborting, Unsuccessful\n";
 		exit;
 	}
+
 	# Remove $convertdir
 	rmtree($convertdir);
 
 	# remove README
-	unlink ("$showdir/README.txt");
+	unlink("$showdir/README.txt");
+
 	# Remove Autologin
 	{
-		local $^I = '.bak';
+		local $^I   = '.bak';
 		local @ARGV = ('/var/lib/vservers/vs1/etc/gdm/autologin');
-		while (<>){
-			unless (m/$machine:$showuser/){
+		while (<>) {
+			unless (m/$machine:$showuser/) {
 				print;
 			}
 		}
-		unlink ('/var/lib/vservers/vs1/etc/gdm/autologin.bak');
+		unlink('/var/lib/vservers/vs1/etc/gdm/autologin.bak');
 	}
+
 	# remove user & $userhome from the system
 	`userdel -r -f $showuser`;
+	`cd /`;
 	`make -C /var/yp/`;
 	print "Uninstall Successful\n";
-	print "Please restart the slideshow machine ($machine) to complete the Uninstallation\n";
+	print
+		"Please restart the slideshow machine ($machine) to complete the Uninstallation\n";
 	exit;
 }
 elsif ($update) {
