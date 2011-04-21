b8c96b21a14111324efbe0e8c26519b0fa726d60
Adam Fairbrother <adam.fairbrother@gmail.com>
2011-04-21 09:32:23 -0700

    Fixed pathing bug with updatecache.pl

--- updatecache.pl
+++ updatecache.pl
@@ -5,35 +5,30 @@ use strict;
 use options;
 
 open my $cache, '>', $xscreencache
-	or die("Cannont open cache file($xscreencache): $!");
+        or die("Cannont open cache file($xscreencache): $!");
 print $cache "$convertdir\n";
 &listfiles( "$convertdir", "", $cache );
 close $cache;
 
 sub listfiles {
 
-	#Output a file list in the format that xscreensaver expects it
-	my ( $dir, $disp, $FH ) = @_;
-	opendir my $show, "$dir";
-	my @dircontent = grep { !/^\./ } readdir($show);
-	closedir($show);
-	foreach (@dircontent) {
-		if ( -d "$dir/$_" ) {
-			if ( $disp eq '' ) {
-				&listfiles( "$dir/$_", "$_", $FH );
-			}
-			else {
-				&listfiles( "$dir/$_", "$disp/$_", $FH );
-			}
-		}
-		elsif ( lc($_) =~ /\.png$/ ) {
-			print $FH "$disp/$_\n";
-		}
-		elsif ( lc($_) =~ /\.gif/ ) {
-			print $FH "$disp/$_\n";
-		}
-		elsif ( lc($_) =~ m/\.jpg/ ) {
-			print $FH "$disp/$_\n";
-		}
-	}
+        #Output a file list in the format that xscreensaver expects it
+        my ( $dir, $disp, $FH ) = @_;
+        opendir my $show, "$dir";
+        my @dircontent = grep { !/^\./ } readdir($show);
+        closedir($show);
+        foreach (@dircontent) {
+                if ( -d "$dir/$_" ) {
+                        &listfiles( "$dir/$_", "$disp/$_", $FH );
+                }
+                elsif ( lc($_) =~ /\.png$/ ) {
+                        print $FH "$disp/$_\n";
+                }
+                elsif ( lc($_) =~ /\.gif/ ) {
+                        print $FH "$disp/$_\n";
+                }
+                elsif ( lc($_) =~ m/\.jpg/ ) {
+                        print $FH "$disp/$_\n";
+                }
+        }
 }
